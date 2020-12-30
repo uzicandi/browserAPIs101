8c78e5ad7326ccef91d19531d4ed134440f82d73
Yona <uzicandi@gmail.com>
2020-12-30 17:50:04 +0900

    stopwatch & random carrot

--- chap09/main.js
+++ chap09/main.js
@@ -1,25 +1,37 @@
 const signBtn = document.querySelector('.signBtn');
+let timeLeft = 10;
 let timerId;
+const remainTime = document.querySelector('.remainTime');
+const container = document.querySelector('.availSpace');
 
 signBtn.addEventListener('click', event => {
+  onClick(event);
+});
+
+function onClick(event) {
   const id = event.target.dataset.id;
   const i = document.querySelector('i');
   i.remove();
   if (id == 'play') {
-    signBtn.innerHTML = `<i class="fas fa-stop"/>`;
+    timerId = setInterval(function() {
+      countdown();
+    }, 1000);
+    for (let i = 0; i < 10; i++) {
+      randomImages();
+    }
+    signBtn.innerHTML = `<i class="fas fa-stop" data-id="stop"/>`;
     signBtn.setAttribute('data-id', 'stop');
-    timerId = setInterval(countdown(), 1000);
   } else {
-    signBtn.innerHTML = `<i class="fas fa-play"/>`;
-    signBtn.setAttribute('data-id', 'play');
     clearTimeout(timerId);
+    remainTime.innerHTML = `<span>00:00</span>`;
+    timeLeft = 10;
+    signBtn.innerHTML = `<i class="fas fa-play" data-id="play"/>`;
+    signBtn.setAttribute('data-id', 'play');
+    container.innerHTML = '';
   }
-});
+}
 
 function countdown() {
-  alert('1');
-  let timeLeft = 10;
-  const remainTime = document.querySelector('.remainTime');
   if (timeLeft == -1) {
     clearTimeout(timerId);
     //doneAlert();
@@ -31,6 +43,21 @@ function countdown() {
     }
     timeLeft--;
   }
+}
+
+function randomImages() {
+  const img = document.createElement('img');
+
+  const availW = container.offsetWidth - 60;
+  const availH = container.offsetHeight - 60;
+  img.src = '/chap09/img/carrot.png';
+
+  let randomY = Math.round(Math.random() * availH) + 'px';
+  let randomX = Math.round(Math.random() * availW) + 'px';
+  img.style.left = randomX;
+  img.style.top = randomY;
+
+  container.appendChild(img);
 }
 
 window.addEventListener('load', () => {});
